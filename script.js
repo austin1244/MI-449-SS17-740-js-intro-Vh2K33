9b8112e9a6a2fd1bd245751eaaa32e92076f8bbf
Austin Rix <rixausti@black1.cse.msu.edu>
2017-01-24 15:25:05 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+(function(){
+  var soundMixer = {
+    init: function(){
+      $("button").mouseenter(soundMixer.play)
+      $("button").click(soundMixer.play)
+    },
+    play: function(e){ 
+      var sound = $(this).data("sound")
+      console.log("play "+ sound)
+      $("#"+sound)[0].play()
+    }
+  }
+  $(document).ready(soundMixer.init)
+
+})()
